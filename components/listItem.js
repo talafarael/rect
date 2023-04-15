24411cfaf8af3413b2a9399209174eae5acea401
BogdanChumak96 <Bogdanghost0@gmail.com>
2023-04-15 22:05:10 +0300

    fix function

--- components/listItem.js
+++ components/listItem.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 
-export default function listItem( { el , deleteProduct }) {
-
+export default function ListItem({ el, deleteProduct }) {
+  // console.log(deleteProduct());
   return (
     <TouchableHighlight onPress={() => deleteProduct(el.key)}>
       <Text style={styles.text}>{el.text}</Text>
     </TouchableHighlight>
   );
-  
-  
 }
 
-const styles = StyleSheet.create({
-
- 
-});
+const styles = StyleSheet.create({});
